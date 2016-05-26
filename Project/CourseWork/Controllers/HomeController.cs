e1520f63854ebc386c9221bd46c86d0cc50835ea
ndmelentev <clava-nik1996@yandex.ru>
2016-05-26 22:51:33 +0300

    renaimed 'code' into 'CourseWork'

---
+++ Project/CourseWork/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace CourseWork.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ Project/CourseWork/Models/TableModels.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace CourseWork.Models
+{
+    public class TableModels 
+    {
+        [Key]
+        public int StudID { get; set; } // Student's ID
+        public int RawScore { get; set; } // Student's score
+        public double Z { get; set; } // Z score
+        public double T { get; set; } // T score
+        public double Perc { get; set; } // Percentile of a student
+        static public TableModels[] tableArray { get; set; }
+    }
+
+    public class TableModelsDbContext : DbContext
+    {
+        public DbSet<TableModels> Students { get; set; }
+    }
+}
